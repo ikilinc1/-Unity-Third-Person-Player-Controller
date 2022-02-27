ee4b4209a8ac34537b78fb8a97feeb33c57edafb
ikilinc1 <ikilinc1@binghamton.edu>
2022-02-27 07:00:38 -0500

    Added movement animations and movement functionality

--- Third-PersonPlayerController/Assets/Crypto/PlayerController.cs
+++ Third-PersonPlayerController/Assets/Crypto/PlayerController.cs
@@ -7,6 +7,20 @@ public class PlayerController : MonoBehaviour
 {
     private Vector2 _moveDirection;
     public float moveSpeed = 2;
+    public float maxForwardSpeed = 8;
+    public float turnSpeed = 100;
+    private float _desiredSpeed;
+    private float _forwardSpeed;
+
+    private const float GroundAccel = 5;
+    private const float GroundDecel = 25;
+
+    private Animator _anim;
+
+    bool IsMoveInput
+    {
+        get { return !Mathf.Approximately(_moveDirection.sqrMagnitude, 0f); }
+    }
     public void OnMove(InputAction.CallbackContext context)
     {
         _moveDirection = context.ReadValue<Vector2>();
@@ -14,12 +28,34 @@ public class PlayerController : MonoBehaviour
 
     void Move(Vector2 direction)
     {
-        transform.Translate(direction.x * moveSpeed * Time.deltaTime, 0, direction.y * moveSpeed * Time.deltaTime);
+        float turnAmount = direction.x;
+        float fDirection = direction.y;
+        
+        if (direction.sqrMagnitude > 1f)
+        {
+            // Normalizes to 1 if value is not between 0 and 1
+            direction.Normalize();
+        }
+        
+        // Get the sign to decide forward or backward
+        _desiredSpeed = direction.magnitude * maxForwardSpeed * Mathf.Sign(fDirection);
+        
+        // if move input is in use accel otherwise decel
+        float acceleration = IsMoveInput ? GroundAccel : GroundDecel;
+
+        // smoothly accelerate or decelerate
+        _forwardSpeed = Mathf.MoveTowards(_forwardSpeed, _desiredSpeed, acceleration * Time.deltaTime);
+        _anim.SetFloat("ForwardSpeed", _forwardSpeed);
+
+        transform.Rotate(0,turnAmount * turnSpeed * Time.deltaTime,0);
+        
+        //transform.Translate(direction.x * moveSpeed * Time.deltaTime, 0, direction.y * moveSpeed * Time.deltaTime);
     }
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        _anim = this.GetComponent<Animator>();
     }
 
     // Update is called once per frame
